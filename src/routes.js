2caaac3c86993c5c5a513f4c53d41511b74f92f9
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-12-24 09:48:28 -0300

    Getting parameters in URL

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,10 @@ module.exports = [
     endpoint: '/users',
     method: 'GET',
     handler: userController.listUsers,
+  },
+  {
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: userController.getUserById,
   }
 ]
