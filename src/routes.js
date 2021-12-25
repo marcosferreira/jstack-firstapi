22780d046c2718e8f2c8da32bdb319f4e4ee4f52
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-12-25 06:54:53 -0300

    Capturing the requests' body

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,10 @@ module.exports = [
     endpoint: '/users/:id',
     method: 'GET',
     handler: userController.getUserById,
+  },
+  {
+    endpoint: '/users',
+    method: 'POST',
+    handler: userController.createUser,
   }
 ]
